1e8b2ab4076dcbc7f0c320daa000459e411497f3
FabricioLopees <prog.fabriciolopes@gmail.com>
2024-03-09 15:09:46 -0300

    build: add script that automate java studies
    This commit implements a new script that is used to facilitate my studies in Java opening the programs I use most

---
+++ study-java.sh
@@ -0,0 +1,4 @@
+# Este script é utilizado para abrir os programas que preciso quando vou estudar programção em Java
+subl ~/Java/aprendendo-java/ &
+evince ~/Java/aprendendo-java/apostila-java-orientacao-objetos.pdf &
+nohup google-chrome-stable https://chat.openai.com > /dev/null 2>&1 &
