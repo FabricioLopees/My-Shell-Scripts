1c97b14a359ff52bece63b77375ea06d8a749cfe
FabricioLopees <prog.fabriciolopes@gmail.com>
2024-03-20 20:28:01 -0300

    Refactor: Add comments and add feature the open apps in background

--- scripts/study-java.sh
+++ scripts/study-java.sh
@@ -1,4 +1,12 @@
-# Este script é utilizado para abrir os programas que preciso quando vou estudar programção em Java
-subl ~/Java/aprendendo-java/ &
-evince ~/Java/aprendendo-java/apostila-java-orientacao-objetos.pdf &
-nohup google-chrome-stable https://chat.openai.com > /dev/null 2>&1 &
+#!/bin/bash
+
+# This script open programs in the background that I use when I study about Java
+
+# Open Sublime Text in my Java directory
+nohup subl ~/Java/Learn-Java/ 1>/dev/null 2>&1 &
+
+# Open Document Viewer
+nohup evince ~/Java/Learn-Java/apostila-java-orientacao-objetos.pdf 1>/dev/null 2>&1 &
+
+# Open Google Chrome with ChatGPT
+nohup google-chrome-stable https://chat.openai.com 1>/dev/null 2>&1 &
