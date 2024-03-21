5e7c300f418e4672d2ad804a294155e1d4f6346f
FabricioLopees <prog.fabriciolopes@gmail.com>
2024-03-21 19:58:15 -0300

    Build: Add feature that create c program
    This commit implements feature that transform the web page in an
    executable file.

--- scripts/cprog.sh
+++ scripts/cprog.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+browser="google-chrome-stable --new-window"
+
+# Verify number of arguments
 if [ $# == 2 ]; then
+	# Create C program
+	echo -e "#include <stdlib.h>\nint main(void){\nsystem(\"$browser $1\");\n}" > $2.c;
+
+	# Compile C code
+	gcc $2.c -o $2;
+
+	# Move C code to a directory
+	mv $2.c ~/Home-Bin/c-programs/
+	mv $2 ~/Home-Bin/
+	
 	# Create webapp.desktop file
 	touch ~/$2.desktop
-	echo -e "[Desktop Entry]\nType=Application\nName=$2\nExec=google-chrome-stable $1\nIcon=$2" > ~/$2.desktop
+	echo -e "[Desktop Entry]\nType=Application\nName=$2\nExec=/home/fabricio/Home-Bin/$2\nIcon=$2" > ~/$2.desktop
 
 	# Move .desktop file
 	sudo mv ~/$2.desktop /usr/share/applications/
 	echo "Successfully created web application!"
+
 else
-	echo "ERROR! Wrong arguments"
-	echo "Sintaxe: $0 [web_link] [desktop_app_name]"
-	1>/dev/null 2>&1
-	exit
+	echo "ERROR!";
+	echo "Sintaxe: cprog [LINK] [APP_NAME]"
+	exit;
 fi
