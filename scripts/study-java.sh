b5688a54ea77a09732cf699a6478869c2a40d6da
FabricioLopees <prog.fabriciolopes@gmail.com>
2024-04-10 02:02:11 -0300

    update: update study-java script
    * switch sublime text to vs code
    * add option to run background
    * change path to open java book
    * change command to open google chrome in chat gtp

--- scripts/study-java.sh
+++ scripts/study-java.sh
@@ -3,10 +3,10 @@
 # This script open programs in the background that I use when I study about Java
 
 # Open Sublime Text in my Java directory
-nohup subl ~/Java/Learn-Java/ 1>/dev/null 2>&1 &
+nohup code 1>/dev/null 2>&1 &
 
 # Open Document Viewer
-nohup evince ~/Java/Learn-Java/apostila-java-orientacao-objetos.pdf 1>/dev/null 2>&1 &
+nohup evince ~/Documents/Sources/Java/apostila-java-orientacao-objetos.pdf 1>/dev/null 2>&1 &
 
 # Open Google Chrome with ChatGPT
-nohup google-chrome-stable https://chat.openai.com 1>/dev/null 2>&1 &
+nohup chatgpt 1>/dev/null 2>&1 &
