c3bd57a5cacc3bafc6f9382c6ccfd5d184c742ce
FabricioLopees <prog.fabriciolopes@gmail.com>
2024-04-12 15:29:00 -0300

    fix variables of the alias script

--- scripts/aa.sh
+++ scripts/aa.sh
@@ -5,7 +5,8 @@
 # Sintaxe: aa [ALIAS NAME] [COMMAND TO EXECUTE]
 
 aliasName=$1;
-aliasCommand=$2;
+shift
+aliasCommand=$@;
 
 echo -e "\n" >>~/.oh-my-bash/custom/aliases/myownaliases.aliases.sh
 echo "alias $aliasName='$aliasCommand'" >> ~/.oh-my-bash/custom/aliases/myownaliases.aliases.sh
