1d483d1127f4e5366c5b5aca1fb125e29e5bc637
Fabricio Lopes <prog.fabriciolopes@gmail.com>
2024-04-13 19:30:22 -0300

    Update scripts (#12)
    * update list of the scripts in the webpage
    
    * update cprog script
    
        - Change path to executable directory
    
    * update rmprog script
    
        - Change path to the executable directory
    
    * rename script: study-java.sh to studyjava.sh

--- scripts/rmprog.sh
+++ scripts/rmprog.sh
@@ -2,10 +2,10 @@
 
 if [ $# == 1 ]; then
 	sudo rm /usr/share/applications/$1.desktop
-	rm ~/Home-Bin/$1
-	rm ~/Home-Bin/c-programs/$1.c
+	rm $HOME/.local/bin/$1
+	rm $HOME/.local/bin/c-programs/$1.c
 	echo "$1 web app successfully removed!"
 else
 	echo "ERROR: Wrong arguments!"
-	echo "Sintaxe: rmprog[APP_NAME]"
+	echo "Sintaxe: rmprog [APP_NAME]"
 fi
